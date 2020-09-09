0a0b6d0bd0611bcb2dd1fd63a852afc1edad0091
unknown <cedricfrancoys@gmail.com>
2020-09-09 09:11:55 +0200

    updates

--- public/packages/blog/apps/home.php
+++ public/packages/blog/apps/home.php
@@ -15,6 +15,10 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-migrate/1.2.1/jquery-migrate.js"></script>
 <script type="text/javascript" src="/packages/blog/assets/js/slick.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<script src="https://cmp.osano.com/Azyw7LSA4W1zR5JN/9f77b139-b3f9-4604-9263-f50cd0c975d0/osano.js"></script>
+
+
 <script>
     $(document).ready( function() {
 
